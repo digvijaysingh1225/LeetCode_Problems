5de2ea1e0d1540b6334096f949fa01a07a4207a8
digvijaysingh1225 <104089746+digvijaysingh1225@users.noreply.github.com>
2022-09-15 21:53:07 +0530

    Time: 2495 ms (5.02%), Space: 74 MB (5.97%) - LeetHub

--- 713-subarray-product-less-than-k/713-subarray-product-less-than-k.java
+++ 713-subarray-product-less-than-k/713-subarray-product-less-than-k.java
@@ -1,35 +1,37 @@
 class Solution {
     public int numSubarrayProductLessThanK(int[] nums, int k) {
         // Time complexity O(n^2) space O(1)
-        // int n = nums.length;
-        // if(n<=1)
-        //     return 0;
-        // int count = 0;
-        // // int prod;
-        // for(int i = 0;i<=n-1;i++){
-        //     int prod = nums[i];
-        //     if(prod<k)
-        //         count++;
-        //     for(int j = i+1;j<=n-1 && prod<k;j++){
-        //         prod*=nums[j];
-        //         if(prod<k)
-        //             count++;
-        //     }
-        // }
-        // return count;
-        
-        if(k<=1)      //nums = [1],k=1
+        int n = nums.length;
+        if(n<=1)
             return 0;
         int count = 0;
-        int left = 0;
-        int prod = 1;
-        for(int r = 0;r<nums.length;r++){
-            prod *= nums[r];
-            while(prod>=k){
-                prod /= nums[left++];
+        // int prod;
+        for(int i = 0;i<=n-1;i++){
+            int prod = nums[i];
+            if(prod<k)
+                count++;
+            for(int j = i+1;j<=n-1 && prod<k;j++){
+                prod*=nums[j];
+                if(prod<k)
+                    count++;
             }
-            count += r-left+1;
         }
         return count;
+        
+        
+        // Optimized Code
+        // if(k<=1)      //nums = [1],k=1
+        //     return 0;
+        // int count = 0;
+        // int left = 0;
+        // int prod = 1;
+        // for(int r = 0;r<nums.length;r++){
+        //     prod *= nums[r];
+        //     while(prod>=k){
+        //         prod /= nums[left++];
+        //     }
+        //     count += r-left+1;
+        // }
+        // return count;
     }
 }
