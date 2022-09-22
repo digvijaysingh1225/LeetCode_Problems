6f53063c7996a49833e6eb15917cabcba2ebb7e8
digvijaysingh1225 <104089746+digvijaysingh1225@users.noreply.github.com>
2022-09-22 08:47:23 +0530

    Time: 2 ms (60.75%), Space: 41.1 MB (84.14%) - LeetHub

---
+++ 119-pascals-triangle-ii/119-pascals-triangle-ii.java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<Integer> getRow(int rowIndex) {
+        List<Integer> row = new ArrayList<>();
+        for(int i = 0; i <= rowIndex; i++){
+            row.add(1);
+            for(int j = i - 1; j > 0; j--){
+                row.set(j, row.get(j-1) + row.get(j));
+            }
+        }
+        return row;
+    }
+}
+
+
+
+
+
+
+
+// For MNC's (Favourite Question)
+// Power(x, n)
+//Pascal's Triangle I and Pascal's Triangle II
