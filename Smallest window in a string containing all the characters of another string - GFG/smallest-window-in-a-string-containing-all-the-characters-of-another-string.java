e65630047908e0e8e6fa1b5573c0be4c8fb54e8e
digvijaysingh1225 <104089746+digvijaysingh1225@users.noreply.github.com>
2022-10-16 19:47:11 +0530

    Added solution - LeetHub

---
+++ Smallest window in a string containing all the characters of another string - GFG/smallest-window-in-a-string-containing-all-the-characters-of-another-string.java
@@ -0,0 +1,66 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main (String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int test = scan.nextInt();
+		
+		while(test > 0){
+		    String s = scan.next();
+		    String t = scan.next();
+		    
+		    System.out.println(new Solution().smallestWindow(s, t));
+		    test--;
+		}
+	}
+}
+// } Driver Code Ends
+
+
+class Solution
+{
+    //Function to find the smallest window in the string s consisting
+    //of all the characters of string p.
+    public static String smallestWindow(String s, String p)
+    {
+        // Your code here
+        HashMap<Character, Integer> map = new HashMap<>();
+        for(int i = 0; i<p.length(); i++){
+            map.put(p.charAt(i), map.getOrDefault(p.charAt(i),0)+1);
+        }
+        int i = 0;
+        int j = 0;
+        String ans = "-1";
+        int min = Integer.MAX_VALUE;
+        int count = map.size();
+        while(j<s.length()){
+            if(map.containsKey(s.charAt(j))){
+                map.put(s.charAt(j), map.get(s.charAt(j))-1);
+                if(map.get(s.charAt(j))==0){
+                    count--;
+                }
+                
+            }
+            if(count==0){
+                while(count == 0){
+                    if(min>j-i+1){
+                            ans = s.substring(i,j+1);
+                            min = Math.min(min,j-i+1);
+                    }
+                    if(map.containsKey(s.charAt(i))){
+                        map.put(s.charAt(i), map.get(s.charAt(i))+1);
+                        if(map.get(s.charAt(i))>0){
+                            count++;
+                        }
+                    }
+                    i++;
+                }
+            }
+            j++;
+        }
+        return ans;
+    }
+}
