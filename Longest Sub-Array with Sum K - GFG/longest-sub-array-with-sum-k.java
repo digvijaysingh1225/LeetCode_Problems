42f2ef9d8a713dd4c54dd1a4d4e713dcf3c16205
digvijaysingh1225 <104089746+digvijaysingh1225@users.noreply.github.com>
2022-10-18 23:17:36 +0530

    Added solution - LeetHub

---
+++ Longest Sub-Array with Sum K - GFG/longest-sub-array-with-sum-k.java
@@ -0,0 +1,79 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+
+
+class Array {
+    
+    // Driver code
+	public static void main (String[] args) throws IOException{
+		// Taking input using buffered reader
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int testcases = Integer.parseInt(br.readLine());
+		
+		// looping through all testcases
+		while(testcases-- > 0){
+		    String line = br.readLine();
+		    String[] element = line.trim().split("\\s+");
+		    int sizeOfArray = Integer.parseInt(element[0]);
+		    int K = Integer.parseInt(element[1]);
+		    
+		    int arr [] = new int[sizeOfArray];
+		    
+		    line = br.readLine();
+		    String[] elements = line.trim().split("\\s+");
+		    for(int i = 0;i<sizeOfArray;i++){
+		        arr[i] = Integer.parseInt(elements[i]);
+		    }
+		    
+		    Solution obj = new Solution();
+		    int res = obj.lenOfLongSubarr(arr, sizeOfArray, K);
+		    
+		    System.out.println(res);
+		}
+	}
+}
+
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    
+   
+    // Function for finding maximum and value pair
+    public static int lenOfLongSubarr (int A[], int N, int K) {
+        //Complete the function
+        int j = 0;
+        int max = 0;
+        int sum = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        while(j < N){
+            sum += A[j];
+            if(sum == K){
+                max = j+1;
+            }
+            if(!map.containsKey(sum)){
+                map.put(sum, j);
+            }
+            
+            if(map.containsKey(sum - K)){
+                max = Math.max(max, j - map.get(sum - K));
+            }
+            j++;
+        }
+        return max;
+    }
+    
+    
+}
+
+
