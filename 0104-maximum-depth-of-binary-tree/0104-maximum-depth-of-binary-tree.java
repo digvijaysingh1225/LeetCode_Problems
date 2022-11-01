d76b4a80b9a6ad1fe94cdf096e1631c80462529f
digvijaysingh1225 <104089746+digvijaysingh1225@users.noreply.github.com>
2022-11-01 18:46:50 +0530

    Time: 0 ms (100.00%), Space: 43.2 MB (43.69%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
@@ -15,40 +15,42 @@
  */
 class Solution {
     public int maxDepth(TreeNode root) {
-        
-        if(root == null){
+        if(root==null){
             return 0;
         }
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.add(root);
-        int ans = 0;
-        while(!queue.isEmpty()){
-            ans++;
-            int n = queue.size();
-            for (int i = 0; i < n; i++) {
-                TreeNode x = queue.remove();
-                if(x.left != null)
-                    queue.add(x.left);
-                if(x.right != null)
-                    queue.add(x.right);
-            }
-        }
-        return ans;
-    
+        int leftdepth = maxDepth(root.left);
+        int rightdepth = maxDepth(root.right);
+        
+        return 1 + Math.max(leftdepth, rightdepth);
         
     }
 }
-
-
-
+// Level Order Traversal Method of calculating the depth of the tree
 
 // public int maxDepth(TreeNode root) {
-//         if(root==null){
+        
+//         if(root == null){
 //             return 0;
 //         }
-//         int leftdepth = maxDepth(root.left);
-//         int rightdepth = maxDepth(root.right);
-        
-//         return 1 + Math.max(leftdepth, rightdepth);
+//         Queue<TreeNode> queue = new LinkedList<>();
+//         queue.add(root);
+//         int ans = 0;
+//         while(!queue.isEmpty()){
+//             ans++;
+//             int n = queue.size();
+//             for (int i = 0; i < n; i++) {
+//                 TreeNode x = queue.remove();
+//                 if(x.left != null)
+//                     queue.add(x.left);
+//                 if(x.right != null)
+//                     queue.add(x.right);
+//             }
+//         }
+//         return ans;
+    
         
 //     }
+
+
+
+
