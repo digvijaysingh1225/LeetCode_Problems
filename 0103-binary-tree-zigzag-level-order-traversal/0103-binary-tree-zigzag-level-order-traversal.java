6d07f27fd4571acd5406ad0f5dc43be90c2f35b6
digvijaysingh1225 <104089746+digvijaysingh1225@users.noreply.github.com>
2022-11-04 21:45:18 +0530

    Time: 1 ms (96.16%), Space: 42.3 MB (76.64%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
@@ -15,25 +15,55 @@
  */
 class Solution {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
-       List<List<Integer>> res =new ArrayList<>();
-        if(root==null)
-            return res;
-        zigzag(root,res,0);
-        return res;
-    }
-    static void zigzag(TreeNode root,List<List<Integer>> res,int level){
-        if(root==null)
-            return;
-        if(res.size()<=level){
-            LinkedList<Integer> lis = new LinkedList<>();
-            res.add(lis);
+        Queue<TreeNode> queue = new LinkedList<>();
+        List<List<Integer>> list = new ArrayList<>();
+        if(root == null){
+            return list;
         }
-            
-        if(level%2==0)
-            res.get(level).add(root.val);
-        else
-            res.get(level).add(0,root.val);
-        zigzag(root.left,res,level+1);
-        zigzag(root.right,res,level+1);
+        queue.offer(root);
+        boolean flag = true;
+        while(!queue.isEmpty()){
+            int levelNum = queue.size();
+            List<Integer> l = new ArrayList<>(levelNum);
+            for(int i = 0; i < levelNum; i++){
+                if(queue.peek().left!=null)
+                    queue.offer(queue.peek().left);
+                if(queue.peek().right!=null)
+                    queue.offer(queue.peek().right);
+                if(flag == true)
+                    l.add(queue.poll().val);
+                else
+                    l.add(0, queue.poll().val);
+            }
+            flag = !flag;
+            list.add(l);
+        }
+        return list;
     }
 }
+
+
+
+
+// public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+//        List<List<Integer>> res =new ArrayList<>();
+//         if(root==null)
+//             return res;
+//         zigzag(root,res,0);
+//         return res;
+//     }
+//     static void zigzag(TreeNode root,List<List<Integer>> res,int level){
+//         if(root==null)
+//             return;
+//         if(res.size()<=level){
+//             LinkedList<Integer> lis = new LinkedList<>();
+//             res.add(lis);
+//         }
+            
+//         if(level%2==0)
+//             res.get(level).add(root.val);
+//         else
+//             res.get(level).add(0,root.val);
+//         zigzag(root.left,res,level+1);
+//         zigzag(root.right,res,level+1);
+//     }
