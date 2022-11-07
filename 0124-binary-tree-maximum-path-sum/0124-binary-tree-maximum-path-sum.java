b2d135a3f43add4e98785c054a1a4aecb0bbb2fd
digvijaysingh1225 <104089746+digvijaysingh1225@users.noreply.github.com>
2022-11-07 19:13:24 +0530

    Time: 1 ms (99.61%), Space: 47.5 MB (64.66%) - LeetHub

--- 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.java
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.java
@@ -14,19 +14,37 @@
  * }
  */
 class Solution {
+    int max = 0;
     public int maxPathSum(TreeNode root) {
-        int[] max = new int[1];
-        max[0] = Integer.MIN_VALUE;
-        maxPath(root, max);
-        return max[0];
+        
+        max = Integer.MIN_VALUE;
+        int val = maxPath(root);
+        return Math.max(max, val);
     }
-    public int maxPath(TreeNode node, int[] sum){
+    public int maxPath(TreeNode node){
         if(node == null){
             return 0;
         }
-        int leftSum = Math.max(0, maxPath(node.left, sum));
-        int rightSum = Math.max(0, maxPath(node.right, sum));
-        sum[0] = Math.max(sum[0], node.val+leftSum+rightSum);
+        int leftSum = Math.max(0, maxPath(node.left));
+        int rightSum = Math.max(0, maxPath(node.right));
+        max = Math.max(max, node.val+leftSum+rightSum);
         return node.val+Math.max(leftSum, rightSum);
     }
 }
+
+
+ // public int maxPathSum(TreeNode root) {
+ //        int[] max = new int[1];
+ //        max[0] = Integer.MIN_VALUE;
+ //        maxPath(root, max);
+ //        return max[0];
+ //    }
+ //    public int maxPath(TreeNode node, int[] sum){
+ //        if(node == null){
+ //            return 0;
+ //        }
+ //        int leftSum = Math.max(0, maxPath(node.left, sum));
+ //        int rightSum = Math.max(0, maxPath(node.right, sum));
+ //        sum[0] = Math.max(sum[0], node.val+leftSum+rightSum);
+ //        return node.val+Math.max(leftSum, rightSum);
+ //    }
