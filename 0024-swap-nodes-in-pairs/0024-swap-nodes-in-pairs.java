c2fc7d347957885ff7d985c5ccb388343e607a8f
digvijaysingh1225 <104089746+digvijaysingh1225@users.noreply.github.com>
2022-11-17 21:48:23 +0530

    Time: 0 ms (100.00%), Space: 41.6 MB (64.58%) - LeetHub

--- 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.java
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.java
@@ -10,20 +10,36 @@
  */
 class Solution {
     public ListNode swapPairs(ListNode head) {
-        ListNode prevNode = new ListNode(0);
-        prevNode.next = head;
-        ListNode newHead = prevNode;
-        while(prevNode.next != null && prevNode.next.next != null){
-            ListNode node1 = prevNode.next;
-            ListNode node2 = node1.next;
-            ListNode nextNode = node2.next;
+//         if(head == null){
+//             return null;
+//         }
+//         ListNode prevNode = new ListNode(0);
+//         prevNode.next = head;
+//         ListNode newHead = prevNode;
+//         while(prevNode.next != null && prevNode.next.next != null){
+//             ListNode n1 = prevNode.next;
+//             ListNode n2 = n1.next;
+//             ListNode nextNode = n2.next;
             
-            prevNode.next = node2;
-            node2.next = node1;
-            node1.next = nextNode;
+//             prevNode.next = n2;
+//             n2.next = n1;
+//             n1.next = nextNode;
             
-            prevNode = node1;
+//             prevNode = n1;
+//         }
+//         return newHead.next;
+        
+        
+        
+        if(head == null || head.next == null){
+            return head;
         }
-        return newHead.next;
+        ListNode p1 = head, p2 = head.next, p3 = p2.next;
+        p1.next = p3;
+        p2.next = p1;
+        if(p3 != null){
+            p1.next = swapPairs(p3);
+        }
+        return p2;
     }
 }
