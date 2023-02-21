2e24ed983b4914ae99125abc0340d104013745e0
digvijaysingh1225 <104089746+digvijaysingh1225@users.noreply.github.com>
2023-02-21 08:49:53 +0530

    Time: 3 ms (96.77%), Space: 42.6 MB (65.73%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        if(s.length() == 0){
+            return true;
+        }
+        int first = 0;
+        int last = s.length()-1;
+        while(first<=last){
+            if(!Character.isLetterOrDigit(s.charAt(first))){
+                first++;
+            }
+            else if(!Character.isLetterOrDigit(s.charAt(last))){
+                last--;
+            }
+            else{
+                if(Character.toLowerCase(s.charAt(first)) != Character.toLowerCase(s.charAt(last))){
+                    return false;
+                }
+                first++;
+                last--;
+            }
+        }
+        return true;
+    }
+}
