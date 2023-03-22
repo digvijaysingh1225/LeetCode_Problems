b0feaf0d4261db4b64301cea4b83d15349f99962
digvijaysingh1225 <104089746+digvijaysingh1225@users.noreply.github.com>
2023-03-22 21:18:17 +0530

    Time: 4 ms (50.17%), Space: 51.7 MB (19.50%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,3 +1,12 @@
+class Pair{
+    int first;
+    int second;
+    public Pair(int first, int second){
+        this.first = first;
+        this.second = second;
+    }
+}
+
 class Solution {
     public int numIslands(char[][] grid) {
         int m=grid[0].length;
@@ -26,4 +35,31 @@ class Solution {
         dfs(i,j-1,vis,grid);
         dfs(i,j+1,vis,grid);
     }
+    
 }
+
+
+// public static void bfs(int i, int j, int[][] vis, char[][] grid){
+//         vis[i][j] = 1;
+//         Queue<Pair> q = new LinkedList<>();
+//         q.add(new Pair(i, j));
+//         int n = grid.length;
+//         int m = grid[0].length;
+//         while(!q.isEmpty()){
+//             int row = q.peek().first;
+//             int col = q.peek().second;
+//             q.remove();
+            
+//             for(int delrow = -1; delrow <= 1; delrow++){
+//                 for(int delcol = -1; delcol <= 1; delcol++){
+//                     int nrow = row + delrow;
+//                     int ncol = col + delcol;
+//                     if(nrow>=0 || nrow<n || ncol>=0 || ncol<m || grid[nrow][ncol]!="0" ||
+//                       vis[nrow][ncol] == 0){
+//                         vis[nrow][ncol] = 1;
+//                         q.add(new Pair(nrow, ncol));
+//                     }
+//                 }
+//             }
+//         }
+//     }
