36ed27873d587ba7c5f07dff6012e18c8f1ccc31
digvijaysingh1225 <104089746+digvijaysingh1225@users.noreply.github.com>
2023-03-30 21:44:37 +0530

    Added solution - LeetHub

--- Bipartite Graph - GFG/bipartite-graph.java
+++ Bipartite Graph - GFG/bipartite-graph.java
@@ -37,43 +37,76 @@ class GFG
 
 class Solution
 {
-    public boolean isBipartite(int V, ArrayList<ArrayList<Integer>>adj)
-    {
-        // Code here
-        int[] color = new int[V];
-        for(int i = 0; i < V; i++){
-            color[i] = -1;
-        }
-        
-        for(int i = 0; i < V; i++){
-            if(color[i] == -1){
-                if(check(i, V, adj, color) == false){
-                    return false;
+     public boolean isBipartite(int V, ArrayList<ArrayList<Integer>>adj){
+         int color[] = new int[V];
+         for(int i = 0; i < V; i++){
+             color[i] = -1;
+         }
+         
+         for(int i = 0; i < V; i++){
+             if(color[i] == -1){
+                if(dfs(adj, color, i, 0) == false){
+                  return false;   
                 }
-            }
-        }
-        return true;
-    }
-    
-    private boolean check(int start, int V, ArrayList<ArrayList<Integer>> adj, int[] color){
-        Queue<Integer> q = new LinkedList<>();
-        q.add(start);
-        color[start] = 0;
-        while(!q.isEmpty()){
-            int node = q.peek();
-            q.remove();
             
-            for(int it : adj.get(node)){
-                if(color[it] == -1){
-                    color[it] = 1 - color[node];
-                    q.add(it);
-                }
-                
-                else if(color[it] == color[node]){
+             }
+         }
+         return true;
+     }
+     private boolean dfs(ArrayList<ArrayList<Integer>>adj, int[] color, int node, int col){
+         color[node] = col;
+         for(int it : adj.get(node)){
+            if(color[it] == -1){
+                if(dfs(adj, color, it, 1-col) == false){
                     return false;
                 }
+                 
+            }else if(color[it] == col){
+                    return false;
             }
-        }
-        return true;
-    }
+         }
+         return true;
+     }
 }
+
+
+//BFS Algo
+//
+//     {
+//         // Code here
+//         int[] color = new int[V];
+//         for(int i = 0; i < V; i++){
+//             color[i] = -1;
+//         }
+        
+//         for(int i = 0; i < V; i++){
+//             if(color[i] == -1){
+//                 if(check(i, V, adj, color) == false){
+//                     return false;
+//                 }
+//             }
+//         }
+//         return true;
+//     }
+    
+//     private boolean check(int start, int V, ArrayList<ArrayList<Integer>> adj, int[] color){
+//         Queue<Integer> q = new LinkedList<>();
+//         q.add(start);
+//         color[start] = 0;
+//         while(!q.isEmpty()){
+//             int node = q.peek();
+//             q.remove();
+            
+//             for(int it : adj.get(node)){
+//                 if(color[it] == -1){
+//                     color[it] = 1 - color[node];
+//                     q.add(it);
+//                 }
+                
+//                 else if(color[it] == color[node]){
+//                     return false;
+//                 }
+//             }
+//         }
+//         return true;
+//     }
