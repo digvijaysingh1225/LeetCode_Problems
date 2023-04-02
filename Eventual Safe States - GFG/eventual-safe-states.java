82a4ea62f825833d9ec58697ead0da1731602229
digvijaysingh1225 <104089746+digvijaysingh1225@users.noreply.github.com>
2023-04-03 00:40:47 +0530

    Added solution - LeetHub

--- Eventual Safe States - GFG/eventual-safe-states.java
+++ Eventual Safe States - GFG/eventual-safe-states.java
@@ -42,42 +42,75 @@ class GFG {
 // User function Template for Java
 
 class Solution {
-
-    List<Integer> eventualSafeNodes(int V, List<List<Integer>> adj) {
-
-        // Your code here
-        int[] vis = new int[V];
-        int[] pathVis = new int[V];
-        int[] check = new int[V];
-        List<Integer> safenodes = new ArrayList<>();
-        
+    List<Integer> eventualSafeNodes(int V, List<List<Integer>> adj){
+        List<List<Integer>> adjRev = new ArrayList<>();
         for(int i = 0; i < V; i++){
-            if(vis[i] == 0){
-                dfsCheck(i, adj, vis, pathVis, check);
+            adjRev.add(new ArrayList<>());
+        }
+        int[] indegree = new int[V];
+        for(int i = 0; i < V; i++){
+            for(int it: adj.get(i)){
+                adjRev.get(it).add(i);
+                indegree[i]++;
             }
         }
+        Queue<Integer> q = new LinkedList<>();
+        List<Integer> safenodes = new ArrayList<>();
         for(int i = 0; i < V; i++){
-            if(check[i] == 1)
-                safenodes.add(i);
+            if(indegree[i] == 0){
+                q.add(i);
+            }
         }
+        while(!q.isEmpty()){
+            int node = q.peek();
+            q.remove();
+            safenodes.add(node);
+            for(int it: adjRev.get(node)){
+                indegree[it]--;
+                if(indegree[it] == 0) q.add(it);
+            }
+        }
+        Collections.sort(safenodes);
         return safenodes;
     }
     
-    private boolean dfsCheck(int node, List<List<Integer>> adj, int[] vis, int[] pathVis, int[] check){
-        vis[node] = 1;
-        pathVis[node] = 1;
-        check[node] = 0;
-        for(int it: adj.get(node)){
-            if(vis[it] == 0){
-                if(dfsCheck(it, adj, vis, pathVis, check) == true)
-                    return true;
-            }
-            else if(pathVis[it] == 1){
-                return true;
-            }
-        }
-        check[node] = 1;
-        pathVis[node] = 0;
-        return false;
-    }
 }
+
+//DFS
+// List<Integer> eventualSafeNodes(int V, List<List<Integer>> adj) {
+
+//         // Your code here
+//         int[] vis = new int[V];
+//         int[] pathVis = new int[V];
+//         int[] check = new int[V];
+//         List<Integer> safenodes = new ArrayList<>();
+        
+//         for(int i = 0; i < V; i++){
+//             if(vis[i] == 0){
+//                 dfsCheck(i, adj, vis, pathVis, check);
+//             }
+//         }
+//         for(int i = 0; i < V; i++){
+//             if(check[i] == 1)
+//                 safenodes.add(i);
+//         }
+//         return safenodes;
+//     }
+    
+//     private boolean dfsCheck(int node, List<List<Integer>> adj, int[] vis, int[] pathVis, int[] check){
+//         vis[node] = 1;
+//         pathVis[node] = 1;
+//         check[node] = 0;
+//         for(int it: adj.get(node)){
+//             if(vis[it] == 0){
+//                 if(dfsCheck(it, adj, vis, pathVis, check) == true)
+//                     return true;
+//             }
+//             else if(pathVis[it] == 1){
+//                 return true;
+//             }
+//         }
+//         check[node] = 1;
+//         pathVis[node] = 0;
+//         return false;
+//     }
