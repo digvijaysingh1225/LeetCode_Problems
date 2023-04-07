2c667a20af1174d947b3965d5120c8eb93d19644
digvijaysingh1225 <104089746+digvijaysingh1225@users.noreply.github.com>
2023-04-07 23:55:05 +0530

    Time: 10 ms (61.79%), Space: 49.8 MB (97.03%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.java
+++ 1020-number-of-enclaves/1020-number-of-enclaves.java
@@ -11,22 +11,23 @@ class Solution {
     public int numEnclaves(int[][] grid) {
         int n = grid.length;
         int m = grid[0].length;
-        
         Queue<Pair> q = new LinkedList<>();
+        
         int[][] vis = new int[n][m];
         for(int i = 0; i < n; i++){
-            for(int j = 0; j<m; j++){
+            for(int j = 0; j < m; j++){
                 if(i==0 || j==0 || i==n-1 || j==m-1){
-                    if(grid[i][j] == 1){
+                    if(grid[i][j] == 1 && vis[i][j] == 0){
                         q.add(new Pair(i, j));
                         vis[i][j] = 1;
+    
                     }
                 }
             }
         }
         
-        int[] delrow = {-1, 0, +1, 0};
-        int[] delcol = {0, +1, 0, -1};
+        int[] delrow = {-1, 0, 1, 0};
+        int[] delcol = {0, 1, 0, -1};
         
         while(!q.isEmpty()){
             int row = q.peek().first;
@@ -34,19 +35,22 @@ class Solution {
             q.remove();
             
             for(int i = 0; i < 4; i++){
-                int nrow = row+delrow[i];
-                int ncol = col+delcol[i];
-                if(nrow>=0 && ncol>=0 && nrow<n && ncol<m && vis[nrow][ncol] == 0 && grid[nrow][ncol] == 1){
+                int nrow = row + delrow[i];
+                int ncol = col + delcol[i];
+                
+                if(nrow>=0 && ncol>=0 && nrow<n && ncol<m && grid[nrow][ncol] == 1 && vis[nrow][ncol] == 0){
                     q.add(new Pair(nrow, ncol));
                     vis[nrow][ncol] = 1;
                 }
             }
         }
+        
         int count = 0;
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < m;j++){
-                if(grid[i][j] == 1 && vis[i][j] == 0){
+        for(int i = 1; i < n-1; i++){
+            for(int j = 1; j < m-1; j++){
+                if(vis[i][j] == 0 && grid[i][j] == 1){
                     count++;
+                    
                 }
             }
         }
