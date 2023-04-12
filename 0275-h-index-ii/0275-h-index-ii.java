c47fdd855aa569e557280b9efc6b0c4107d60f61
digvijaysingh1225 <104089746+digvijaysingh1225@users.noreply.github.com>
2023-04-13 00:32:55 +0530

    Time: 0 ms (100.00%), Space: 49.7 MB (40.14%) - LeetHub

---
+++ 0275-h-index-ii/0275-h-index-ii.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int hIndex(int[] citations) {
+        int len = citations.length;
+        int start = 0;
+        int end = citations.length -1;
+        while(start<=end){
+            int mid = (end + start)/2;
+            if(citations[mid] == len - mid){
+                return len - mid;
+            }else if(citations[mid] < len - mid){
+                start = mid + 1;
+            }else{
+                end = mid - 1;
+            }
+        }
+        return len - start;
+    }
+}
