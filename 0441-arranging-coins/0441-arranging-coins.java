2efaa6f2056017f9fb827c169bc6435684dd5436
digvijaysingh1225 <104089746+digvijaysingh1225@users.noreply.github.com>
2023-04-13 13:09:36 +0530

    Time: 1 ms (100.00%), Space: 40.4 MB (20.12%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.java
+++ 0441-arranging-coins/0441-arranging-coins.java
@@ -8,6 +8,22 @@ class Solution {
 //         }
 //         return i-1;
         
-        return (int)(Math.sqrt(2*(long)n + 0.25) - 0.5);
+        // return (int)(Math.sqrt(2*(long)n + 0.25) - 0.5);
+        
+        long left = 0;
+        long right = n;
+        long ans = 0;
+            
+        while(left <= right){
+            long mid = (right + left)>>1;
+            if((mid)*(mid+1)/2 <=n){
+                ans = mid;
+                left = mid+1;
+            }
+            else{
+                right = mid -1;
+            }
+        }
+        return (int)ans;
     }
 }
