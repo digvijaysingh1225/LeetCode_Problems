00b87864626843bb25f8cf9be3e6632832c324a8
digvijaysingh1225 <104089746+digvijaysingh1225@users.noreply.github.com>
2023-04-19 01:47:22 +0530

    Time: 1 ms (99.25%), Space: 43.7 MB (29.99%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,4 +1,5 @@
 class Solution {
+   
     public int findCircleNum(int[][] M) {
         
         int[] vis = new int[M.length];
@@ -20,7 +21,6 @@ class Solution {
             }
         }
     }
-        
 }
     
     
@@ -61,5 +61,108 @@ class Solution {
 //             if(vis[it]==0){
 //                 dfs(it, vis, adjList);
 //             }
+//         }
+//     }
+
+
+
+
+
+//Other method
+// public int findCircleNum(int[][] M) {
+        
+//         int[] vis = new int[M.length];
+//         int count = 0;
+//         for(int i = 0; i < M.length; i++){
+//             if(vis[i] == 0){
+//                 dfs(i, vis, M);
+//                 count++;
+//             }
+//         }
+//         return count;
+//     }
+    
+//     public void dfs(int i, int[] vis, int[][] M){
+//         for(int j = 0; j < M.length; j++){
+//             if(M[i][j] == 1 && vis[j] == 0){
+//                 vis[j] = 1;
+//                 dfs(j, vis, M);
+//             }
+//         }
+//     }
+
+
+//Disjoint method not working currently
+// public int findCircleNum(int[][] M){
+//        int n = M.length;
+//        DisjointSet ds = new DisjointSet(n);
+//        for(int i = 0; i < n; i++){
+//            for(int j = 0; j < n; j++){
+//                if(M[i][j] == 1){
+//                    ds.unionBySize(i, j);
+//                }
+//            }
+//        }
+//        int cnt = 0;
+//        for(int i = 0; i < n; i++){
+//            if(ds.parent.get(i) == i) cnt++;
+           
+//        }
+//        return cnt;
+//    }
+        
+// }
+// class DisjointSet{
+//     List<Integer> rank = new ArrayList<>();
+//     List<Integer> parent = new ArrayList<>();
+//     List<Integer> size = new ArrayList<>();
+//     public DisjointSet(int n){
+//         for(int i = 0; i <= n; i++){
+//             rank.add(0);
+//             parent.add(i);
+//         }
+//     }
+    
+//     public int findUPar(int node){
+//         if(node == parent.get(node)){
+//             return node;
+//         }
+//         int ulp = findUPar(parent.get(node));
+//         parent.set(node, ulp);
+//         return parent.get(node);
+//     }
+    
+//     public void unionByRank(int u, int v){
+//         int ulp_u = findUPar(u);
+//         int ulp_v = findUPar(v);
+//         if(ulp_u == ulp_v){
+//             return;
+//         }
+//         if(rank.get(ulp_u) < rank.get(ulp_v)){
+//             parent.set(ulp_u, ulp_v);
+//         }
+//         else if(rank.get(ulp_v) < rank.get(ulp_v)){
+//             parent.set(ulp_v, ulp_u);
+//         }
+//         else{
+//             parent.set(ulp_v, ulp_u);
+//             int rankU = rank.get(ulp_u);
+//             rank.set(ulp_u, rankU + 1);
+//         }
+//     }
+    
+//     public void unionBySize(int u, int v){
+//         int ulp_u = findUPar(u);
+//         int ulp_v = findUPar(v);
+//         if(ulp_u == ulp_v){
+//             return;
+//         }
+//         if(size.get(ulp_u) < size.get(ulp_v)){
+//             parent.set(ulp_u, ulp_v);
+//             size.set(ulp_v, size.get(ulp_v) + size.get(ulp_u));
+//         }
+//         else{
+//             parent.set(ulp_v, ulp_u);
+//             size.set(ulp_u, size.get(ulp_u) + size.get(ulp_v));
 //         }
 //     }
