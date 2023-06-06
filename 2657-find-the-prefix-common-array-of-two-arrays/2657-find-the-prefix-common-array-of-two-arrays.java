9da5f9bd08873abedad4de62e2b8eb84ec552826
digvijaysingh1225 <104089746+digvijaysingh1225@users.noreply.github.com>
2023-06-06 11:48:28 +0530

    Time: 4 ms (80.90%), Space: 44.6 MB (5.74%) - LeetHub

--- 2657-find-the-prefix-common-array-of-two-arrays/2657-find-the-prefix-common-array-of-two-arrays.java
+++ 2657-find-the-prefix-common-array-of-two-arrays/2657-find-the-prefix-common-array-of-two-arrays.java
@@ -2,18 +2,33 @@ class Solution {
     public int[] findThePrefixCommonArray(int[] A, int[] B) {
         int n = A.length;
         int[] c = new int[n];
-        
+        Set<Integer> set = new HashSet<>();
         for(int i = 0; i < n; i++){
-            boolean found = false;
-            for(int j = 0; j < n; j++){
-                if(A[i] == B[j]){
-                    found = true;                    
-                }
-                if(found && j >= i){
-                    c[j]++;
-                }
+            set.add(A[i]);
+            set.add(B[i]);
+            if(set.size() != (2*(i+1))){
+                c[i] = (2*(i+1)) - set.size();
             }
         }
         return c;
     }
 }
+
+
+
+
+// int n = A.length;
+//         int[] c = new int[n];
+        
+//         for(int i = 0; i < n; i++){
+//             boolean found = false;
+//             for(int j = 0; j < n; j++){
+//                 if(A[i] == B[j]){
+//                     found = true;                    
+//                 }
+//                 if(found && j >= i){
+//                     c[j]++;
+//                 }
+//             }
+//         }
+//         return c;
