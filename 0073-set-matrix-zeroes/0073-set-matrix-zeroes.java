85c6499a7badb3cac80373ec2e23f51b3509d70c
digvijaysingh1225 <104089746+digvijaysingh1225@users.noreply.github.com>
2023-07-06 22:08:27 +0530

    Time: 1 ms (75.06%), Space: 44.8 MB (28.06%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -3,37 +3,40 @@ class Solution {
         int n = matrix.length;
         int m = matrix[0].length;
         
-        int[] row = new int[m];
-        int[] col = new int[n];
-        
+        int col0 = 1;
         for(int i = 0; i < n; i++){
             for(int j = 0; j < m; j++){
                 if(matrix[i][j] == 0){
-                    row[j] = 1;
-                    col[i] = 1;
+                    matrix[i][0] = 0;
+                    if(j != 0){
+                        matrix[0][j] = 0;
+                    }
+                    else{
+                        col0 = 0;
+                    }
                 }
             }
         }
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
-                if(row[j] == 1 || col[i] == 1){
-                    matrix[i][j] = 0;
+        for(int i = 1; i < n; i++){
+            for(int j = 1; j < m; j++){
+                if(matrix[i][j] != 0){
+                    if(matrix[0][j] == 0 || matrix[i][0] == 0){
+                        matrix[i][j] = 0;
+                    }
                 }
             }
-        }        
+        }
+        if(matrix[0][0] == 0){
+            for(int j = 0; j < m; j++){
+                matrix[0][j] = 0;
+            }
+        }
+        if(col0 == 0){
+            for(int i = 0; i < n; i++){
+                matrix[i][0] = 0;
+            }
+        }
+        
     }
-//     public void setnegative(int[][] matrix, int row, int col, int n, int m){
-        
-//         for(int i = 0; i < m; i++){
-//             if(matrix[row][i] != 0){
-//                 matrix[row][i] = -1;
-//             }
-//         }
-        
-//         for(int j = 0; j < n; j++){
-//             if(matrix[j][col] != 0){
-//                 matrix[j][col] = -1;
-//             }
-//         }
-//     }
+
 }
