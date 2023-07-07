0d888ec1a3d32b6318aa12afffd0c4d60dc49c0c
digvijaysingh1225 <104089746+digvijaysingh1225@users.noreply.github.com>
2023-07-07 23:06:50 +0530

    Time: 0 ms (100.00%), Space: 41.5 MB (53.51%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -1,19 +1,34 @@
 class Solution {
     public void rotate(int[][] matrix) {
-        for(int i = 0; i < matrix.length; i++){
-            for(int j = i; j < matrix[0].length; j++){
+        int n = matrix.length;
+        int m = matrix[0].length;
+        
+        transpose(matrix);
+        
+        swapRows(matrix);
+        
+    }
+    
+    public void transpose(int[][] A){
+        int n = A.length;
+        for(int i = 0; i < n; i++){
+            for(int j = i; j < n; j++){
+                
                 int temp = 0;
-                temp = matrix[i][j];
-                matrix[i][j] = matrix[j][i];
-                matrix[j][i] = temp;
+                temp = A[i][j];
+                A[i][j] = A[j][i];
+                A[j][i] = temp;
             }
         }
-        for(int i = 0; i < matrix.length; i++){
-            for(int j = 0; j < matrix.length/2; j++){
+    }
+    public void swapRows(int[][] A){
+        int n = A.length;
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n/2; j++){
                 int temp = 0;
-                temp = matrix[i][j];
-                matrix[i][j] = matrix[i][matrix.length - 1 - j];
-                matrix[i][matrix.length - 1 - j] = temp;
+                temp = A[i][j];
+                A[i][j] = A[i][n - j - 1];
+                A[i][n - j - 1] = temp;
             }
         }
     }
