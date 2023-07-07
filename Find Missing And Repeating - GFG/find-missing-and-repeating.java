526fd36ca77936b9d25f79093d86daf3f6945b3c
digvijaysingh1225 <104089746+digvijaysingh1225@users.noreply.github.com>
2023-07-07 13:51:20 +0530

    Added solution - LeetHub

--- Find Missing And Repeating - GFG/find-missing-and-repeating.java
+++ Find Missing And Repeating - GFG/find-missing-and-repeating.java
@@ -32,26 +32,49 @@ class GFG {
 class Solve {
     int[] findTwoElement(int arr[], int n) {
         // code here
+        
+        //Method 1
+        // int[] res = new int[2];
+        
+        // int[] temp = new int[n];
+        // int repeating = -1;
+        // int missing = -1;
+        // for(int i = 0; i < n; i++){
+        //     temp[arr[i] - 1]++;
+        //     if(temp[arr[i] - 1] > 1){
+        //         repeating = arr[i];
+        //     }
+        //     res[0] = repeating;
+        
+        // }
+        // for(int i = 0; i < n; i++){
+        //     if(temp[i] == 0){
+        //         missing = i+1;
+        //     }
+        //     res[1] = missing;
+        // }
+        
+        // return res;
+        
+        
+        
+        //Method 2
         int[] res = new int[2];
         
-        int[] temp = new int[n];
-        int repeating = -1;
-        int missing = -1;
         for(int i = 0; i < n; i++){
-            temp[arr[i] - 1]++;
-            if(temp[arr[i] - 1] > 1){
-                repeating = arr[i];
+            int abs = Math.abs(arr[i]);
+            if(arr[abs - 1] > 0){
+                arr[abs - 1] = -arr[abs - 1];
             }
-            res[0] = repeating;
-        
+            else{
+                res[0] = abs;
+            }
         }
         for(int i = 0; i < n; i++){
-            if(temp[i] == 0){
-                missing = i+1;
+            if(arr[i] > 0){
+                res[1] = i+1;
             }
-            res[1] = missing;
         }
-        
         return res;
     }
 }
