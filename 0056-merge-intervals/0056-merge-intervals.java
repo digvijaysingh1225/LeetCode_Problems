0990d4bc6367138c7f0109572be4afe075ae9fa7
digvijaysingh1225 <104089746+digvijaysingh1225@users.noreply.github.com>
2023-07-10 21:39:11 +0530

    Time: 8 ms (97.26%), Space: 45.4 MB (89.20%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,15 +1,14 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        
-        
         if(intervals.length < 2){
             return intervals;
         }
         Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+        
         List<int[]> list = new ArrayList<>();
         int[] current = intervals[0];
         list.add(current);
-        for(int[] next : intervals){
+        for(int[] next: intervals){
             if(current[1] >= next[0]){
                 current[1] = Math.max(current[1], next[1]);
             }
@@ -19,8 +18,5 @@ class Solution {
             }
         }
         return list.toArray(new int[(list.size())][]);
-        
-        
-        
     }
 }
