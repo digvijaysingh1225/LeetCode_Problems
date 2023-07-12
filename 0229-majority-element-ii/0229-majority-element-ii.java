7ee900370e4f7268f0f405ae9ee8e6a803d704a3
digvijaysingh1225 <104089746+digvijaysingh1225@users.noreply.github.com>
2023-07-12 17:42:48 +0530

    Time: 3 ms (61.19%), Space: 47.1 MB (28.34%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,40 +1,39 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        int num1 = -1, num2 = -1, cnt1 = 0, cnt2 = 0, len = nums.length;
-        for(int i = 0; i < len; i++){
-            if(nums[i] == num1)
-                cnt1++;
-            else if(nums[i] == num2)
-                cnt2++;
-            else if(cnt1 == 0){
-                num1 = nums[i];
+        int cnt1 = 0;
+        int cnt2 = 0;
+        int ele1 = Integer.MIN_VALUE;
+        int ele2 = Integer.MIN_VALUE;
+        for(int i = 0; i < nums.length; i++){
+            if(cnt1 == 0 && nums[i] != ele2){
                 cnt1 = 1;
+                ele1 = nums[i];
             }
-            else if(cnt2 == 0){
-                num2 = nums[i];
+            else if(cnt2 == 0 && nums[i] != ele1){
                 cnt2 = 1;
+                ele2 = nums[i];
             }
+            else if(nums[i] == ele1) cnt1++;
+            else if(nums[i] == ele2) cnt2++;
             else{
                 cnt1--;
                 cnt2--;
             }
         }
-        List<Integer> ans = new ArrayList<>();
-        cnt1 = 0;
-        cnt2 = 0;
-        for(int i=0; i < len; i++){
-            if(nums[i] == num1){
-                cnt1++;
-            }
-            else if(nums[i] == num2){
-                cnt2++;
-            }
+        List<Integer> list = new ArrayList<>();
+        int c1 = 0, c2 = 0;
+        for(int i = 0; i < nums.length; i++){
+            if(ele1 == nums[i]) c1++;
+            if(ele2 == nums[i]) c2++;
         }
-        if(cnt1 > len/3)
-            ans.add(num1);
-        if(cnt2 > len/3)
-            ans.add(num2);
-        
-        return ans;
+        int min = (nums.length/3)+1;
+        System.out.println(min);
+        if(c1 >= min){
+            list.add(ele1);
+        }
+        if(c2 >= min){
+            list.add(ele2);
+        }
+        return list;
     }
 }
