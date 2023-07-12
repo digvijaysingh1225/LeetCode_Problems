ae80eb32ab7eb2a09d614a88ffa646c4a05eade8
digvijaysingh1225 <104089746+digvijaysingh1225@users.noreply.github.com>
2023-07-12 22:05:27 +0530

    Time: 0 ms (100.00%), Space: 39.3 MB (68.13%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,12 +1,35 @@
 class Solution {
-    public int uniquePaths(int m, int n) {
-        int N = n + m -2;
-        int r = m - 1;
-        double res = 1;
-        for(int i = 1; i <= r; i++){
-            res = res * (N - r + i)/i;
-            
+    public int uniquePaths(int m, int n){
+        int[][] dp = new int[m][n];
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                dp[i][j] = -1;
+            }
         }
-        return (int)res;
+        int ans = countPaths(0, 0, m, n, dp);
+        return ans;
+    }
+    private int countPaths(int i, int j, int m, int n, int[][] dp){
+        if(i == m-1 && j == n-1) return 1;
+        if(i>=m || j>=n) return 0;
+        
+        if(dp[i][j] != -1) return dp[i][j];
+        else return dp[i][j] = countPaths(i+1, j, m, n, dp) + countPaths(i, j+1, m, n, dp);
     }
 }
+
+
+
+//this solution gives TLE 
+
+// public int uniquePaths(int m, int n) {
+//         int i = 0;
+//         int j = 0;
+//         int ans = countPaths(i, j, m, n);
+//         return ans;
+//     }
+//     private int countPaths(int i, int j, int m, int n){
+//         if(i == m-1 && j == n-1) return 1;
+//         if(i>=m || j>=n) return 0;
+//         else return countPaths(i+1, j, m, n) + countPaths(i, j+1, m, n);
+//     }
