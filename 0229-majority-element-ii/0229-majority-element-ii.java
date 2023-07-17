d57bacb36ca343b908bb50cac893aed5b44df846
digvijaysingh1225 <104089746+digvijaysingh1225@users.noreply.github.com>
2023-07-17 21:47:23 +0530

    Time: 2 ms (97.89%), Space: 47 MB (29.59%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,39 +1,79 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        int cnt1 = 0;
-        int cnt2 = 0;
-        int ele1 = Integer.MIN_VALUE;
-        int ele2 = Integer.MIN_VALUE;
-        for(int i = 0; i < nums.length; i++){
-            if(cnt1 == 0 && nums[i] != ele2){
-                cnt1 = 1;
+        int n = nums.length;
+        List<Integer> res = new ArrayList<>();
+        
+        int count1 = 0, count2 = 0;
+        int ele1 = Integer.MIN_VALUE, ele2 = Integer.MIN_VALUE;
+        
+        for(int i = 0; i < n; i++){
+            if(count1 == 0 && nums[i] != ele2){
+                count1 = 1;
                 ele1 = nums[i];
             }
-            else if(cnt2 == 0 && nums[i] != ele1){
-                cnt2 = 1;
+            else if(count2 == 0 && nums[i] != ele1){
+                count2 = 1;
                 ele2 = nums[i];
             }
-            else if(nums[i] == ele1) cnt1++;
-            else if(nums[i] == ele2) cnt2++;
+            else if(nums[i] == ele1) count1++;
+            else if(nums[i] == ele2) count2++;
             else{
-                cnt1--;
-                cnt2--;
+                count1--;
+                count2--;
             }
         }
-        List<Integer> list = new ArrayList<>();
-        int c1 = 0, c2 = 0;
-        for(int i = 0; i < nums.length; i++){
-            if(ele1 == nums[i]) c1++;
-            else if(ele2 == nums[i]) c2++;
+        int cnt1 = 0;
+        int cnt2 = 0;
+        for(int i = 0; i < n; i++){
+            if(nums[i] == ele1){
+                cnt1++;
+            }
+            else if(nums[i] == ele2){
+                cnt2++;
+            }
         }
-        int min = (nums.length/3)+1;
-        System.out.println(min);
-        if(c1 >= min){
-            list.add(ele1);
+        if(cnt1 > n/3){
+            res.add(ele1);
         }
-        if(c2 >= min){
-            list.add(ele2);
+        if(cnt2 > n/3){
+            res.add(ele2);
         }
-        return list;
+        
+        return res;
     }
+    // int cnt1 = 0;
+    //     int cnt2 = 0;
+    //     int ele1 = Integer.MIN_VALUE;
+    //     int ele2 = Integer.MIN_VALUE;
+    //     for(int i = 0; i < nums.length; i++){
+    //         if(cnt1 == 0 && nums[i] != ele2){
+    //             cnt1 = 1;
+    //             ele1 = nums[i];
+    //         }
+    //         else if(cnt2 == 0 && nums[i] != ele1){
+    //             cnt2 = 1;
+    //             ele2 = nums[i];
+    //         }
+    //         else if(nums[i] == ele1) cnt1++;
+    //         else if(nums[i] == ele2) cnt2++;
+    //         else{
+    //             cnt1--;
+    //             cnt2--;
+    //         }
+    //     }
+    //     List<Integer> list = new ArrayList<>();
+    //     int c1 = 0, c2 = 0;
+    //     for(int i = 0; i < nums.length; i++){
+    //         if(ele1 == nums[i]) c1++;
+    //         else if(ele2 == nums[i]) c2++;
+    //     }
+    //     int min = (nums.length/3)+1;
+    //     System.out.println(min);
+    //     if(c1 >= min){
+    //         list.add(ele1);
+    //     }
+    //     if(c2 >= min){
+    //         list.add(ele2);
+    //     }
+    //     return list;
 }
